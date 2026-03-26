ffb12619a0bbb584c24b08b70ee900c35edb5cb1
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-03-27 06:01:38 +0800

    Fix mixins (again)

--- src/main/java/net/kamaar/tmixinsmod/mixin/PrimordialCrestArmorItemMixin.java
+++ src/main/java/net/kamaar/tmixinsmod/mixin/PrimordialCrestArmorItemMixin.java
@@ -2,30 +2,27 @@ package net.kamaar.tmixinsmod.mixin;
 
 import com.gametechbc.gtbcs_geomancy_plus.api.init.GGAttributes;
 import com.gametechbc.traveloptics.item.armor.PrimordialCrestArmorItem;
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraftforge.registries.RegistryObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = PrimordialCrestArmorItem.class, remap = false)
+@Mixin(value = PrimordialCrestArmorItem.class)
 public abstract class PrimordialCrestArmorItemMixin {
 
-    @ModifyArg(
+    @ModifyReceiver(
             method = "triggerKnockbackAndDamage",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraftforge/registries/RegistryObject;get()Ljava/lang/Object;",
-                    ordinal = 0
+                    target = "Lnet/minecraftforge/registries/RegistryObject;get()Ljava/lang/Object;"
             ),
             remap = false
     )
-    private Object redirectAttributeGet(RegistryObject<?> instance) {
+    private RegistryObject redirectAttributeGet(RegistryObject instance) {
         if (instance == AttributeRegistry.NATURE_SPELL_POWER) {
-            return GGAttributes.GEO_SPELL_POWER.get();
+            return GGAttributes.GEO_SPELL_POWER;
         }
-        return instance.get();
+        return instance;
     }
 }
