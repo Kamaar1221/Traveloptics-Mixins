71f912bbcbf0b0b52156e241f1a8cc8d8f0a1a10
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-03-27 05:26:47 +0800

    Fix mixins

--- src/main/java/net/kamaar/tmixinsmod/mixin/AbyssalHideArmorItemMixin.java
+++ src/main/java/net/kamaar/tmixinsmod/mixin/AbyssalHideArmorItemMixin.java
@@ -1,37 +1,40 @@
 package net.kamaar.tmixinsmod.mixin;
 
-import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
+import com.gametechbc.traveloptics.item.armor.AbyssalHideArmorItem;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.acetheeldritchking.cataclysm_spellbooks.registries.CSAttributeRegistry;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(targets = "com.gametechbc.traveloptics.item.armor.AbyssalHideArmorItem")
+@Mixin(value = AbyssalHideArmorItem.class, remap = false)
 public abstract class AbyssalHideArmorItemMixin {
 
-    @Redirect(
+    @ModifyArg(
             method = "calculateAbyssOrbDamage",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D",
-                    ordinal = 0
-            )
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D", ordinal = 0)
     )
-    private double redirectEnderSpellPower(LivingEntity entity, Attribute attribute) {
-        return entity.getAttributeValue(CSAttributeRegistry.ABYSSAL_MAGIC_POWER.get());
+    private Attribute redirectEnderSpellPowerA(Attribute pAttribute) {
+        return CSAttributeRegistry.ABYSSAL_MAGIC_POWER.get();
     }
 
-    @Redirect(
+    @ModifyArg(
             method = "calculateAbyssOrbDamage",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D",
-                    ordinal = 1
-            )
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/LivingEntity;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D", ordinal = 1)
     )
-    private double redirectEldritchSpellPower(LivingEntity entity, Attribute attribute) {
-        return 0.0D;
+    private Attribute redirectEnderSpellPowerB(Attribute pAttribute) {
+        return CSAttributeRegistry.ABYSSAL_MAGIC_POWER.get();
+    }
+
+
+    @ModifyReturnValue(
+            method = "calculateAbyssOrbDamage",
+            at = @At("RETURN")
+    )
+    private float redirectEnderSpellPowerC(float original) {
+        return original / 2;
     }
 }
